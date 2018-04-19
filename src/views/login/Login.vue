4c83436eff23f0729a73b0da7e5df550035dde14
zhourunbin <1875222156@qq.com>
2018-04-20 00:33:56 +0800

    vue-admin

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -4,7 +4,7 @@
     <el-row>
       <el-col>
         <div class="content">
-          <el-form>
+          <el-form v-loading="false" :element-loading-text="'正在登录中...'" :rules="rule_data">
 
             <el-form-item prop="username">
               <el-input type="text" auto-complete="off" placeholder="账号"></el-input>
@@ -28,9 +28,36 @@
 </template>
 
 <script>
-    export default {
-        name: "Login"
+  export default {
+    name: "Login",
+    data() {
+      return {
+        data: {
+          username: '',
+          password: ''
+        },
+
+        rule_data: {
+          username: [{
+            validator: (rule, value, callback) => {
+              if (value === '') {
+                callback(new Error('请输入用户名'))
+              } else {
+                if (/^[a-zA-Z0-9_-]{1,16}$/.test(value)) {
+                  callback()
+                } else {
+                  callback(new Error('用户名至少6位,由大小写字母和数字,-,_组成'))
+                }
+              }
+            },
+            trigger: 'blur'
+          }],
+        }
+
+      }
+
     }
+  }
 </script>
 
 <style scoped>
